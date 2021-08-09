97ef8249a4aebdc870edb477d282103d07b644f4
dayle998 <dayle998@naver.com>
2021-08-09 20:50:08 +0900

    feat:URL링크

--- src/main/java/min/thymeleafbasic/basic/BasicController.java
+++ src/main/java/min/thymeleafbasic/basic/BasicController.java
@@ -73,6 +73,13 @@ public class BasicController {
 
     }
 
+    @GetMapping("link")
+    private String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
     @Data
     static class User {
         private String username;
