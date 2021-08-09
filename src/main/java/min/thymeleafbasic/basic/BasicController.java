e529797f14c8c9441f5c8217aba3279c5b0fb9db
dayle998 <dayle998@naver.com>
2021-08-09 20:04:43 +0900

    feat:타임리프 다양한 변수 표현식

--- src/main/java/min/thymeleafbasic/basic/BasicController.java
+++ src/main/java/min/thymeleafbasic/basic/BasicController.java
@@ -1,8 +1,17 @@
 package min.thymeleafbasic.basic;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -11,14 +20,61 @@ public class BasicController {
     @RequestMapping("/text-basic")
     public String textBasic(Model model) {
         model.addAttribute("data", "Study ThymeLeaf");
-        return "/basic/text-basic";
+        return "basic/text-basic";
     }
 
     @RequestMapping("/text-unescaped")
     public String textUnEscaped(Model model) {
         model.addAttribute("data", "<b>Study ThymeLeaf</b>");
-        return "/basic/text-unescaped";
+        return "basic/text-unescaped";
     }
 
-    
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA  = new User("userA", 10);
+        User userB  = new User("userB", 20);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+
+
+    //편의 객체에서 사용
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Session Data");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello" + data;
+        }
+    }
+
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
+    }
+
+
 }
