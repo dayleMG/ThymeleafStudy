4b2cb43b847081ad27d0bfd330c5f3b518ece663
dayle998 <dayle998@naver.com>
2021-08-10 18:29:24 +0900

    feat:append, checked 속성 값 처리

--- src/main/java/min/thymeleafbasic/basic/BasicController.java
+++ src/main/java/min/thymeleafbasic/basic/BasicController.java
@@ -92,7 +92,12 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/operation";
     }
-    
+
+    @GetMapping("/attribute")
+    public String atrribute() {
+        return "basic/attribute";
+    }
+
     @Data
     static class User {
         private String username;
