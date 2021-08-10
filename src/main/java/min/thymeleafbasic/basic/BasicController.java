07cf8efd3b3095a036a33bd8116ca6b485673fba
dayle998 <dayle998@naver.com>
2021-08-10 19:51:26 +0900

    feat:th:each 반복문 사용법

--- src/main/java/min/thymeleafbasic/basic/BasicController.java
+++ src/main/java/min/thymeleafbasic/basic/BasicController.java
@@ -94,10 +94,26 @@ public class BasicController {
     }
 
     @GetMapping("/attribute")
-    public String atrribute() {
+    public String attribute() {
         return "basic/attribute";
     }
-    
+
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    public void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+
+        model.addAttribute("users", list);
+    }
+
+
     @Data
     static class User {
         private String username;
